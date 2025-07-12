e2ab302859f5e775511e620a4545ed820c6045bb
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-12 03:39:16 +0300

    implemented variables

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+    $kategori1= "Macera";
+    $kategori2= "Dram";
+    $kategori3= "Komedi";
+    $kategori4= "Korku";
+
+    $filmBaslik1 = "Paper Lives";
+    $filmOzet1 = "Kağıt toplayarak geçinen ve sağlığı giderek kötüleşen Mehmet terk edilmiş bir çocuk bulur. Birden hayatına giren küçük Ali, onu kendi çocukluğuyla yüzleştirecektir";
+    $filmResim1 = "1.jpeg";
+    $filmYorumSayisi1 = "Yorum: 55";
+    $filmBegeniSayisi1 = "Beğeni: 85";
+    $filmVizyon1 = "Vizyonda: Evet";
+
+    $filmBaslik2 = "The Walking Dead";
+    $filmOzet2 = "Zombi kıyametinin ardından hayatta kalanlar, birlikte verdikleri ölüm kalım mücadelesinde insanlığa karşı duydukları umuda tutunur.";
+    $filmResim2 = "2.jpeg";
+    $filmYorumSayisi2 = "Yorum: 45";
+    $filmBegeniSayisi2 = "Beğeni: 95";
+    $filmVizyon2 = "Vizyonda: Evet";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,28 +36,30 @@
         <div class="row">
             <div class="col-3">
                 <ul class="list-group">
-                    <li class="list-group-item">Macera</li>
-                    <li class="list-group-item">Dram</li>
-                    <li class="list-group-item">Komedi</li>
-                    <li class="list-group-item">Korku</li>
+                    <li class="list-group-item"><?php echo $kategori1?></li>
+                    <li class="list-group-item"><?php echo $kategori2?></li>
+                    <li class="list-group-item"><?php echo $kategori3?></li>
+                    <li class="list-group-item"><?php echo $kategori4?></li>
                 </ul>
             </div>
             <div class="col-9">
                 <div class="card mb-3">
                     <div class="row">
                         <div class="col-3">
-                            <img class="img-fluid" src="img/1.jpeg" alt="">
+                            <?php echo
+                            "<img class=\"img-fluid\" src=\"img/{$filmResim1}\">"
+                            ?>
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Paper Lives</h5>
-                                <p class="card-text">Kağıt toplayarak geçinen ve sağlığı giderek kötüleşen Mehmet terk edilmiş bir çocuk bulur. Birden hayatına giren küçük Ali, onu kendi çocukluğuyla yüzleştirecektir. (18 yaş ve üzeri için uygundur)</p>
+                                <h5 class="card-title"><?php echo $filmBaslik1 ?> </h5>
+                                <p class="card-text"><?php echo $filmOzet1 ?></p>
                             </div>
                             <span>
                                 <div class="badge bg-success">Yapım Tarihi: 03.12.2021</div>
-                                <div class="badge bg-success">Yorum: 55</div>
-                                <div class="badge bg-success">Beğeni: 85</div>
-                                <div class="badge bg-success">Vizyonda: Evet</div>
+                                <div class="badge bg-success"><?php echo $filmYorumSayisi1 ?></div>
+                                <div class="badge bg-success"><?php echo $filmBegeniSayisi1 ?></div>
+                                <div class="badge bg-success"><?php echo $filmVizyon1 ?></div>
                             </span>
                         </div>
                     </div>
@@ -42,18 +67,20 @@
                 <div class="card mb-3">
                     <div class="row">
                         <div class="col-3">
-                            <img class="img-fluid" src="img/2.jpeg" alt="">
+                            <?php echo
+                            "<img class=\"img-fluid\" src=\"img/{$filmResim2}\">"
+                            ?>
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Walking Dead</h5>
-                                <p class="card-text">Zombi kıyametinin ardından hayatta kalanlar, birlikte verdikleri ölüm kalım mücadelesinde insanlığa karşı duydukları umuda tutunur.</p>
+                                <h5 class="card-title"><?php echo $filmBaslik2 ?> </h5>
+                                <p class="card-text"><?php echo $filmOzet2 ?></p>
                             </div>
                             <span>
                                 <div class="badge bg-success">Yapım Tarihi: 31.10.2010</div>
-                                <div class="badge bg-success">Yorum: 45</div>
-                                <div class="badge bg-success">Beğeni: 95</div>
-                                <div class="badge bg-success">Vizyonda: Evet</div>
+                                <div class="badge bg-success"><?php echo $filmYorumSayisi2 ?></div>
+                                <div class="badge bg-success"><?php echo $filmBegeniSayisi2 ?></div>
+                                <div class="badge bg-success"><?php echo $filmVizyon2 ?>t</div>
                             </span>
                         </div>
                     </div>
