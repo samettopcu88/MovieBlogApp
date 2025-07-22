09afb4f6e945b10d30c549aa5911967675de2338
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-22 21:39:20 +0300

    cookies processes are done

--- views/_navbar.php
+++ views/_navbar.php
@@ -12,7 +12,7 @@ if (!empty($_GET['q'])) {
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-        <a href="/blog-app" class="navbar-brand">BlogApp</a>
+        <a href="/" class="navbar-brand">BlogApp</a>
 
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -25,6 +25,27 @@ if (!empty($_GET['q'])) {
                 <li class="nav-item">
                     <a href="#" class="nav-link">Link 3</a>
                 </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+
+                <?php if (isset($_COOKIE["auth"])): ?>
+                    <li class="nav-item">
+                        <a href="logout.php" class="nav-link">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Hoşgeldiniz, <?php echo $_COOKIE["auth"]["name"] ?></a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Register</a>
+                    </li>
+                <?php endif; ?>
+
+
+
             </ul>
 
             <form action="index.php" class="d-flex" method="GET">
